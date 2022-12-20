aec400107a61539313566cff3d9b1380e0931af1
SP <stephenparkhum@outlook.com>
2022-12-20 13:53:00 -0800

    Added new packages and a googleconnection test

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const schedule = require("node-schedule");
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
@@ -9,10 +11,24 @@ const client = require("twilio")(accountSid, authToken);
 const testSenderNum = process.env.TEST_SENDER_NUM;
 const testUserNum = process.env.TEST_USER_NUM;
 
-client.messages
+const testMessage = () => {
+  console.log("This message is running now...");
+};
+
+// Get all the stuff at midnight
+const job = schedule.scheduleJob("32 * * * *", () => testMessage());
+
+// @TODO #1
+// Add "midnight" gathering of calendar events
+
+// @TODO #2
+// Add scheduling of each calendar event
+
+/* client.messages
   .create({
     body: "THIS SHIT ACTUALLY WORKS",
     from: `+${testSenderNum}`,
     to: `+${testUserNum}`,
   })
   .then((message) => console.log(message.sid));
+*/
