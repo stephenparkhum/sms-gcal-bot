f70b7e0f4c3cac4006b0df63852a1769faafc77d
SP <stephenparkhum@outlook.com>
2022-12-20 13:14:13 -0800

    Adding ts stuff for the index

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-// BOILERPLATE GARBAGE
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-//
+require("dotenv").config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require("twilio")(accountSid, authToken);
+
+// TEST CREDS
 const testSenderNum = process.env.TEST_SENDER_NUM;
 const testUserNum = process.env.TEST_USER_NUM;
-const client = require("twilio")(accountSid, authToken);
 
 client.messages
   .create({
